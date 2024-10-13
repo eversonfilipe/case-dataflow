03eafe7f0cdc91924e10e32dadc7f3719e3b1355
Éverson Filipe <eversonfilipe124@gmail.com>
2024-10-13 00:19:52 -0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -6,3 +6,17 @@ readMoreButtons.forEach(button => {
     expandedContent.style.display = expandedContent.style.display === 'none' ? 'block' : 'none';
   });
 });
+
+const scrollTopBtn = document.getElementById('scroll-top-btn');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    scrollTopBtn.classList.add('show');
+  } else {
+    scrollTopBtn.classList.remove('show');
+  }
+});
+
+scrollTopBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
